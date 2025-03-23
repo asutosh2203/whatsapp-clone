638607832d6d56644cc9dfa83eb73711e6cfaf82
asutosh2203 <asutosh2001@gmail.com>
2025-03-23 23:50:31 +0530

    Shifted the creating of new chat functionality into the sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,7 +8,18 @@ import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
 import db, { auth } from './firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  orderBy,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  serverTimestamp,
+  onSnapshot,
+} from 'firebase/firestore';
+import { getUserRefByEmail } from './utils';
 import { signOut } from 'firebase/auth';
 
 export default function Sidebar() {
@@ -16,20 +27,10 @@ export default function Sidebar() {
   const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
-    // const roomsRef = collection(db, 'rooms');
-
-    // const unsubscribe = onSnapshot(roomsRef, (snapshot) => {
-    //   setRooms(
-    //     snapshot.docs.map((doc) => ({
-    //       id: doc.id,
-    //       data: doc.data(),
-    //     }))
-    //   );
-    // });
-
     const chatsRef = collection(db, 'chats');
+    const q = query(chatsRef, orderBy('lastUpdated', 'desc')); // Query with ordering
 
-    const unsubscribe = onSnapshot(chatsRef, (snapshot) => {
+    const unsubscribe = onSnapshot(q, (snapshot) => {
       setChats(
         snapshot.docs.map((doc) => {
           if (doc.id.includes(user.uid)) {
@@ -46,17 +47,65 @@ export default function Sidebar() {
     return () => unsubscribe();
   }, []);
 
+  const createChat = async () => {
+    const recipientEmail = prompt('Please enter email of recipient');
+    if (!recipientEmail) return;
+
+    try {
+      const userRef = await getUserRefByEmail(recipientEmail);
+      const userSnap = await getDoc(userRef);
+
+      if (!userSnap.exists()) {
+        throw new Error('Recipient not found');
+      }
+
+      const recipientUid = userSnap.data().uid;
+      const currentUid = user.uid;
+
+      if (recipientEmail == currentUid) {
+        throw new Error("You can't use your email as recipient's ID");
+      }
+
+      const chatId =
+        recipientUid > currentUid
+          ? recipientUid + '_' + currentUid
+          : currentUid + '_' + recipientUid;
+
+      // console.log('Chat ID:', chatId);
+
+      // Check if the chat already exists
+      const chatRef = doc(db, 'chats', chatId);
+      const chatSnap = await getDoc(chatRef);
+
+      if (!chatSnap.exists()) {
+        await setDoc(chatRef, {
+          chatId,
+          participants: [currentUid, recipientUid],
+          lastMessage: '',
+          lastUpdated: serverTimestamp(),
+        });
+
+        console.log(`New chat with ${recipientEmail} created successfully!`);
+      } else {
+        console.log('Chat already exists');
+      }
+    } catch (error) {
+      console.error('Error creating chat room:', error);
+    }
+  };
+
   return (
     <div className='sidebar'>
       <div className='sidebar_header'>
         <Avatar>
           <img className='user_avatar' src={user?.photoURL} />
         </Avatar>
+        <p>{user?.displayName}</p>
         <div className='sidebar_headerRight'>
           <IconButton>
             <DonutLargeIcon />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={createChat}>
             <AddOutlinedIcon />
           </IconButton>
           <IconButton>
@@ -75,10 +124,7 @@ export default function Sidebar() {
         </div>
       </div>
       <div className='sidebar_chats'>
-        <SidebarChat addNewChat />
         {chats.map((chatData) => {
-          console.log(chatData);
-
           return (
             chatData && (
               <SidebarChat
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -17,12 +17,7 @@ import {
 import { useStateValue } from './StateProvider';
 import { getUserRefByEmail, getOtherUserId } from './utils';
 
-export default function SidebarChat({
-  addNewChat,
-  id,
-  lastUpdated,
-  lastMessage,
-}) {
+export default function SidebarChat({ id, lastUpdated, lastMessage }) {
   const [messages, setMessages] = useState('');
   const [name, setName] = useState('');
   const [photo, setPhoto] = useState('');
@@ -35,7 +30,7 @@ export default function SidebarChat({
       const recipientRef = doc(db, 'users', recipientId);
       const recipientSnap = await getDoc(recipientRef);
 
-      console.log(recipientSnap.data());
+      // console.log(recipientSnap.data());
 
       setName(recipientSnap.data().name);
       setPhoto(recipientSnap.data().photoURL);
@@ -51,71 +46,28 @@ export default function SidebarChat({
     }
   }, [id]);
 
-  const createChat = async () => {
-    const recipientEmail = prompt('Please enter email of recipient');
-    if (!recipientEmail) return;
+  const lastUpdatedFormatted =
+    new Date(lastUpdated?.toDate()).toLocaleTimeString().split(':')[0] +
+    ':' +
+    new Date(lastUpdated?.toDate()).toLocaleTimeString().split(':')[1];
 
-    try {
-      const userRef = await getUserRefByEmail(recipientEmail);
-      const userSnap = await getDoc(userRef);
-
-      if (!userSnap.exists()) {
-        throw new Error('Recipient not found');
-      }
-
-      const recipientUid = userSnap.data().uid;
-      const currentUid = user.uid;
-
-      if (recipientEmail == currentUid) {
-        throw new Error("You can't use your email as recipient's ID");
-      }
-
-      const chatId =
-        recipientUid > currentUid
-          ? recipientUid + '_' + currentUid
-          : currentUid + '_' + recipientUid;
-
-      // console.log('Chat ID:', chatId);
-
-      // Check if the chat already exists
-      const chatRef = doc(db, 'chats', chatId);
-      const chatSnap = await getDoc(chatRef);
-
-      if (!chatSnap.exists()) {
-        await setDoc(chatRef, {
-          chatId,
-          participants: [currentUid, recipientUid],
-          lastMessage: '',
-          lastUpdated: serverTimestamp(),
-        });
-
-        console.log(`New chat with ${recipientEmail} created successfully!`);
-      } else {
-        console.log('Chat already exists');
-      }
-    } catch (error) {
-      console.error('Error creating chat room:', error);
-    }
-  };
-
-  return !addNewChat ? (
+  return (
     <Link to={`/chats/${id}`}>
       <div className='sidebarChat'>
-        <Avatar
-          alt='prof-pic'
-          variant='circular'
-          children={<img className='chat_avatar' src={photo} />}
-        />
+        <div className='sidebarChat_left'>
+          <Avatar
+            alt='prof-pic'
+            variant='circular'
+            children={<img className='chat_avatar' src={photo} />}
+          />
 
-        <div className='sidebarChat_info'>
-          <h4>{name}</h4>
-          <p>{lastMessage ? lastMessage : 'New chat'}</p>
+          <div className='sidebarChat_info'>
+            <h4>{name}</h4>
+            <p>{lastMessage ? lastMessage : 'New chat'}</p>
+          </div>
         </div>
+        <p className='last_updated'>{lastUpdatedFormatted}</p>
       </div>
     </Link>
-  ) : (
-    <div onClick={createChat} className='sidebarChat'>
-      <h2>Add New Chat</h2>
-    </div>
   );
 }
